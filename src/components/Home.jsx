cd8653d10caacbfe5c37ab159dce5dd7ba0d0c33
Nancy Emad <nancyemadd0099@gmail.com>
2026-02-08 20:24:21 +0200

    add device support section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,29 @@
 import './Home.css'
 import GenreCard from './GenreCard'
+import DeviceSupport from './homeContent/DeviceSupport';
 
 function Home() {
 
     return (
         <>
             <section className='Home-section'>
-
-                <div className="categories">
+                <div className="categories-section">
                     <div className="categories-header">
                         <p>Explore our wide variety of categories</p>
                         <p>Whether you're looking for a comedy to make you laugh, a drama to make you think, or a documentary to learn something new</p>
                     </div>
-
-                    <div className="categories-section">
-                        <div className="categories-grid">
-                            <GenreCard title="Action" genreId={28} />
-                            <GenreCard title="Adventure" genreId={12} />
-                            <GenreCard title="Comedy" genreId={35} />
-                            <GenreCard title="Drama" genreId={18} />
-                            <GenreCard title="Horror" genreId={27} />
-                        </div>
+                    <div className="categories-grid">
+                        <GenreCard title="Action" genreId={28} />
+                        <GenreCard title="Adventure" genreId={12} />
+                        <GenreCard title="Comedy" genreId={35} />
+                        <GenreCard title="Drama" genreId={18} />
+                        <GenreCard title="Horror" genreId={27} />
                     </div>
                 </div>
 
-
-
+                <div className="device-support-section">
+                    <DeviceSupport />
+                </div>
             </section>
         </>
     )
---
+++ src/components/homeContent/DeviceSupport.jsx
@@ -0,0 +1,42 @@
+import './DeviceSupport.css'
+
+function DeviceSupport() {
+
+    return (
+        <div className="device-support">
+            <div className="device-support-header">
+                <p>We Provide you streaming experience across various devices.</p>
+                <p>With MoveScope, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.</p>
+            </div>
+
+            <div className="device-support-cards">
+                <div className="device-card">
+                    <p><i class="fa-solid fa-mobile-button"></i>Smartphones</p>
+                    <p>MoveScope is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+                </div>
+                <div className="device-card">
+                    <p><i class="fa-solid fa-tablet"></i>Tablet</p>
+                    <p>MoveScope is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+                </div>
+                <div className="device-card">
+                    <p><i class="fa-solid fa-tv"></i>Smart TV</p>
+                    <p>MoveScope is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+                </div>
+                <div className="device-card">
+                    <p><i class="fa-solid fa-laptop"></i>Laptops</p>
+                    <p>MoveScope is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+                </div>
+                <div className="device-card">
+                    <p><i class="fa-solid fa-gamepad"></i>Gaming Consoles</p>
+                    <p>MoveScope is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+                </div>
+                <div className="device-card">
+                    <p><i class="fa-solid fa-headset"></i>VR Headsets </p>
+                    <p>MoveScope is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DeviceSupport;
