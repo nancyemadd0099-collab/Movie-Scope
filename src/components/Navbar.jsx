217ebb39a0f383434ddc6bee18bf9fc9b3246e89
Nancy Emad <nancyemadd0099@gmail.com>
2026-02-15 21:07:30 +0200

    add responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom'
 import './Navbar.css'
 
 function Navbar() {
-    const location= useLocation ()
+    const location = useLocation()
+    const [menuOpen, setMenuOpen] = useState(false)
+
     useEffect(() => {
         const nav = document.querySelector('.navbar-section')
         if (location.pathname === '/') {
@@ -16,10 +18,10 @@ function Navbar() {
                 }
             }
             window.addEventListener('scroll', handleScroll)
-            return ()=>window.removeEventListener('scroll',handleScroll)
+            return () => window.removeEventListener('scroll', handleScroll)
         }
         nav.classList.add('scrolled')
-    },[location.pathname])
+    }, [location.pathname])
 
     return (
         <>
@@ -28,15 +30,25 @@ function Navbar() {
                     <div className="logo">
                         <p><span className="logo-letter">M</span>ovieScope</p>
                     </div>
-                    <div className="nav-links">
-                        <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
-                        <Link to="/Movies" className={location.pathname === '/Movies' ? 'active' : ''}>Movies & Shows</Link>
-                        <Link to="/Support" className={location.pathname === '/Support' ? 'active' : ''}>Support</Link>
-                        <Link to="/Subscriptions" className={location.pathname === '/Subscriptions' ? 'active' : ''}>Subscriptions</Link>
+                    <div className={`nav-links ${menuOpen ? 'open' : ''}`}>
+                        <Link to="/" className={location.pathname === '/' ? 'active' : ''} onClick={()=>setMenuOpen(false)}>Home</Link>
+                        <Link to="/Movies" className={location.pathname === '/Movies' ? 'active' : ''} onClick={()=>setMenuOpen(false)}>Movies & Shows</Link>
+                        <Link to="/Support" className={location.pathname === '/Support' ? 'active' : ''} onClick={()=>setMenuOpen(false)}>Support</Link>
+                        <Link to="/Subscriptions" className={location.pathname === '/Subscriptions' ? 'active' : ''} onClick={()=>setMenuOpen(false)}>Subscriptions</Link>
                     </div>
                     <div className="nav-actions">
                         <p className='search-icon'><i class="fa-solid fa-magnifying-glass"></i></p>
                         <button><a href="#">Login</a></button>
+                    </div>
+
+                    {/* humburger Button */}
+                    <div
+                        className={`menu-btn ${menuOpen ? 'active' : ''}`}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
                 </div>
             </nav>
