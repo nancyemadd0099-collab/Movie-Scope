3101a44d2047230ece15e0185d14aed755515e7c
Nancy Emad <nancyemadd0099@gmail.com>
2026-04-07 10:06:05 +0200

    change image background about hero section

--- src/components/homeContent/Hero.jsx
+++ src/components/homeContent/Hero.jsx
@@ -1,18 +1,39 @@
 
+import { useEffect } from 'react';
 import './Hero.css'
 import { Link } from 'react-router-dom'
 
 function Hero() {
-    
+
+    useEffect(() => {
+        const spotlight = document.querySelector(".spotlight")
+
+        const move = (e) => {
+            const x = e.clientX;
+            const y = e.clientY;
+
+            spotlight.style.makeImage =
+                `radial-gradient(circle 420px at ${x}px ${y}px, transparent 0%, black 60%)`;
+
+            spotlight.style.webkitMaskImage =
+                `radial-gradient(circle 420px at ${x}px ${y}px, transparent 0%, black 60%)`;
+
+        };
+        window.addEventListener("mousemove", move);
+
+        return () => window.removeEventListener("mousemove", move)
+    }, [])
+
     return (
         <>
             <section id='hero-section'>
-                    <div className="content-hero">
-                        <h1>The Best <br /> Streaming Experience</h1>
-                        <p>MovieScope is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With MovieScope. <br />You can also create your own watchlists, so you can easily find the content you want to watch.</p>
-                        <Link to="/Movies"><button> <i class="fa-solid fa-play"></i> Start Watching Now</button></Link>
-                    </div>
-        </section>
+                <div className="spotlight"></div>
+                <div className="content-hero">
+                    <h1>The Best <br /> Streaming Experience</h1>
+                    <p>MovieScope is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With MovieScope. <br />You can also create your own watchlists, so you can easily find the content you want to watch.</p>
+                    <Link to="/Movies"><button> <i class="fa-solid fa-play"></i> Start Watching Now</button></Link>
+                </div>
+            </section>
         </>
     )
 }
